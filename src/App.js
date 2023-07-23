140175cc7b69acd760e9add67ff6475b1b7fc1d4
sourab-jadav <sourabhjadav7@gmail.com>
2023-07-23 18:24:36 +0530

    remove clutter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log('app');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>hello world</h1>
+        <Demo></Demo>
+      </header>
+    </div>
+  );
+}
+function Demo() {
+  console.log(`demo`);
+  return (
+    <div className="App">
+      
+      <header className="App-header">
+        <h1>Demo</h1>
       </header>
     </div>
   );
